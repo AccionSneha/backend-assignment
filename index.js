dda4081210b6df5d7b59257fca1347432abbe2b4
ACCIONLABS\al3235 <sneha.lanjewar@accionlabs.com>
2020-11-28 13:19:39 +0530

    index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const environment = process.env.NODE_ENV;
 const app = require("./config/express");
 const port = process.env.PORT || config.port;
 
-// const dbService = require("./api/services/db.service");
-// const DB = dbService(environment, config.migrate).start();
+const dbService = require("./api/services/db.service");
+const DB = dbService(environment, config.migrate).start();
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port} in ${environment} mode`);
-
-  // return DB;
+  return DB;
 });
 
 module.exports = app;
