cf43215af488d79729e5fbac9c2c61881238901b
ACCIONLABS\al3235 <sneha.lanjewar@accionlabs.com>
2020-12-03 16:22:30 +0530

    create public folder for hosting static files

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const port = process.env.PORT;
 const dbService = require("./api/services/db.service");
 const DB = dbService(environment, config.migrate).start();
 
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.use(express.static(path.join(__dirname, "app")));
 app.use("/static", express.static(path.join(__dirname, "app", "static")));
 app.use("/favicon.ico", express.static("favicon.ico"));
 app.get("/", (req, res) => {
-  return res.sendFile(
-    path.join(__dirname, "app", "templates", "index_page.html")
-  );
+  return res.sendFile(path.join(__dirname, "public", "index_page.html"));
 });
+// app.get("/", (req, res) => {
+//   return res.sendFile(
+//     path.join(__dirname, "app", "templates", "index_page.html")
+//   );
+// });
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
   return DB;
