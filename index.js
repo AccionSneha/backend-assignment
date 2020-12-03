2b97285c1b28137bbbf1e1c5649cdc8b2e2be4ae
ACCIONLABS\al3235 <sneha.lanjewar@accionlabs.com>
2020-12-03 17:38:23 +0530

    update index.js file for static directory name

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const port = process.env.PORT;
 const dbService = require("./api/services/db.service");
 const DB = dbService(environment, config.migrate).start();
 
-app.use(express.static(path.join(__dirname, "app")));
-app.use("/static", express.static(path.join(__dirname, "app", "static")));
+process.env.PWD = process.cwd();
+
+// console.log(path.join(process.env.PWD, "app", "static"));
+
+app.use(express.static(path.join(process.env.PWD, "app")));
+app.use("/static", express.static(path.join(process.env.PWD, "app", "static")));
 app.use("/favicon.ico", express.static("favicon.ico"));
 app.get("/", (req, res) => {
   return res.sendFile(
-    path.join(__dirname, "app", "templates", "index_page.html")
+    path.join(process.env.PWD, "app", "templates", "index_page.html")
   );
 });
 
