5de821161d2ebd7c8b300375a98c498fae039b06
ACCIONLABS\al3235 <sneha.lanjewar@accionlabs.com>
2020-12-03 15:59:24 +0530

    update static routes

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ const dbService = require("./api/services/db.service");
 const DB = dbService(environment, config.migrate).start();
 
 app.use(express.static(path.join(__dirname, "app")));
+app.use("/static", express.static(path.join(__dirname, "app", "static")));
 app.use("/favicon.ico", express.static("favicon.ico"));
-// app.get("/ui", (req, res) => {
-//   return res.sendFile(
-//     path.join(__dirname, "app", "templates", "index_page.html")
-//   );
-// });
 app.get("/", (req, res) => {
   return res.sendFile(
     path.join(__dirname, "app", "templates", "index_page.html")
