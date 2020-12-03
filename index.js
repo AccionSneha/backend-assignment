6334ac7e2421932a1c29d4f2c83f5b6fbe50adbe
ACCIONLABS\al3235 <sneha.lanjewar@accionlabs.com>
2020-12-03 18:15:24 +0530

    update default path

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const port = process.env.PORT;
 const dbService = require("./api/services/db.service");
 const DB = dbService(environment, config.migrate).start();
 
-// app.use("/public", express.static(path.join(__dirname, "app", "static")));
-// app.use("/templates", express.static(path.join(__dirname, "app", "templates")));
-// app.use("/favicon.ico", express.static("favicon.ico"));
-// app.get("/", (req, res) => {
-//   return res.sendFile(
-//     path.join(__dirname, "app", "templates", "index_page.html")
-//   );
-// });
-
-app.use(express.static("public"));
-
-// define the first route
-app.get("/", function (req, res) {
-  return res.sendFile(path.join(__dirname, "public", "index_page.html"));
+app.use("/static", express.static(path.join(__dirname, "app", "static")));
+app.use("/templates", express.static(path.join(__dirname, "app", "templates")));
+app.use("/favicon.ico", express.static("favicon.ico"));
+app.get("/*", (req, res) => {
+  return res.sendFile(
+    path.join(__dirname, "app", "templates", "index_page.html")
+  );
 });
+
+// app.use("/public", express.static("public"));
+
+// // define the first route
+// app.get("/", function (req, res) {
+//   return res.sendFile(path.join(__dirname, "public", "index_page.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
